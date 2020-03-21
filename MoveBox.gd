bd434b4b75ef52fc17105db092fc35280377cb36
Garbaj <62218238+GarbajYT@users.noreply.github.com>
2020-03-21 17:11:00 -0400

    Add files via upload

---
+++ MoveBox.gd
@@ -0,0 +1,22 @@
+extends KinematicBody
+
+var speed = 20
+var point = Vector3(52, 12, 0)
+
+func _process(delta):
+
+	var direction
+
+	if point.distance_to(transform.origin) > 0.05:
+		direction = point - transform.origin
+		direction = direction.normalized() * speed
+
+	else:
+		direction = point - transform.origin
+
+	move_and_slide(direction) 
+		
+		
+#	transform.origin = lerp(transform.origin, point, speed * delta)
+
+	
